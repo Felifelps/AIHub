e7764ddcfa2bf5475a74b176a4b6b4a55f9cd92e
Felifelps <felipefelipe23456@gmail.com>
2025-01-29 10:13:32 -0300

    streamlit deploy fixes

--- utils/set_up_sqlite.py
+++
@@ -1,7 +0,0 @@
-def set_up_sqlite():
-    import os
-    os.environ["LD_LIBRARY_PATH"] = "/home/appuser/.local/bin"
-
-    __import__('pysqlite3')
-    import sys
-    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
